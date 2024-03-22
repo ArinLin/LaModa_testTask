e938ec457aeecac4f1d755812cf24743f395ef7f
Arina <arinaligit@gmail.com>
2024-03-22 20:14:10 +0300

    add logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,16 @@
 package main
 
+import (
+	"github.com/urfave/cli/v2"
+)
+
+var globalFlags = []cli.Flag{
+	&cli.StringFlag{
+		Name:    "log-level",
+		Usage:   "log level",
+		EnvVars: []string{"LOG_LEVEL"},
+		Value:   "local",
+	},
+}
+
 func main() {}
